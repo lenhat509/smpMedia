df26a2f5908dd069d5372477e4eb76784f9138c2
lenhat <leconganh8436@gmail.com>
2020-05-28 16:18:06 -0700

    delete

--- Project2/FlaskProject/posts/form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100)])
-    content = StringField('Content', validators=[DataRequired(), Length(min=10, max=1000)])
-    submit= SubmitField('Submit')
